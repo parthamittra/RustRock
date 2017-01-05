e7cc1a7ce81eaeef23183152f7b9cc34a6a64856
EsportsHero <partha@esportshero.com>
2017-01-04 22:12:29 -0500

    added webserver to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use] extern crate nickel;
+
+use nickel::Nickel;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Welcome to Rust Rock!  This is a template generator written in Rust."
+        }
+    });
+
+    server.listen("127.0.0.1:6767");
 }
